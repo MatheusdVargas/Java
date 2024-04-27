0382a42d22e3c46b8e0b9cdc749a8b1c20d5a4d9
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-04-27 20:44:07 -0300

    Add files via upload

---
+++ Iniciação/MeusCalculos.java
@@ -0,0 +1,7 @@
+public class MeusCalculos {
+    public static void main(String[] args ) {
+        System.out.println("Meus Calculos:");
+        System.out.println("Subtraçao de 1247 - 351. " + ("\nResultado: ") + (1247-351));
+        System.out.println("Multiplicaçao de 391 por 14. " + ("\nResultado: ") + (391 * 14));
+    }
+}
