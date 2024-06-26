4951756ad2a4437424ccbb0a2fcddd627d1140c5
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-06-26 16:52:29 -0300

    Add files via upload

---
+++ Lista3/ExercicioTreze.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author 05138300067
+ */
+public class ExercicioTreze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
