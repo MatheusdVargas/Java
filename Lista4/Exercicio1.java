b7a69d71f515848be27055051d53831cf382243f
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-06-26 14:24:29 -0300

    Update Exercicio1.java

--- Lista4/Exercicio1.java
+++ Lista4/Exercicio1.java
@@ -18,9 +18,9 @@ public class Exercicio1 {
         double numF = Math.pow(2, 7) - Math.pow(3, 4);
         System.out.println(Math.cbrt(numF));
         double numG = Math.pow(9, 7) * Math.pow(4, 3);
-        System.out.println(numG); // problema
+        System.out.println(Math.pow(numG, 1.0/5);
         double numH = Math.pow(2, 5) / Math.pow(5, 2);
-        System.out.println(Math.pow(numH, 1/4)); // problema
+        System.out.println(Math.pow(numH, 1.0/4));
     }
     
 }
