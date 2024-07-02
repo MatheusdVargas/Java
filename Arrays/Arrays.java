d0185abfcf2461a6d53ef2dd023a8c88b6914b6f
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-07-02 20:20:45 -0300

    Add files via upload

---
+++ Arrays/Arrays.java
@@ -0,0 +1,17 @@
+/*
+ Crie uma array e faça sua leitura empriminndo-a na tela.
+ */
+package arrays;
+/**
+ *
+ * @author Matheus V
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        int n[] = {3, 5, 4, 8, 1, 3};
+        for(int c = 0; c <= 5; c++) {
+            System.out.println("O numero na posicao " + c + " eh " + n[c]);
+        }
+    }
+    
+}
