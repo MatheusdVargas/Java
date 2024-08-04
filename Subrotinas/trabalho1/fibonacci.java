a55dffdf3b85b76dc28034d7aa22ba363bad3c15
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-08-04 14:58:51 -0300

    Update fibonacci.java

--- Subrotinas/trabalho1/fibonacci.java
+++ Subrotinas/trabalho1/fibonacci.java
@@ -3,7 +3,6 @@ Criar um método para calcular o n-ésimo número da série de Fibonacci.
 A série inicia com os valores 1 e 1, e os números subsequentes
 são a soma dos dois anteriores. (ex.: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...)
  */
-package metodos01;
 
 public class fibonacci {
    public static void main(String[] args) {
