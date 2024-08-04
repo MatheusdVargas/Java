3a72c24ef598f52fd8b3af26ca6370d8ac49f426
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-08-04 14:24:02 -0300

    Update equacao.java

--- Subrotinas/trabalho1/equacao.java
+++ Subrotinas/trabalho1/equacao.java
@@ -1,19 +1,45 @@
+/*
+Criar um método para calcular as raízes reais de uma equação do 2º grau. - feito
+Os parâmetros de entrada são os coeficientes a, b e c. - feito
+O retorno é um array contendo as duas raízes (se houver apenas 1 raiz, o array terá 1 posição;
+se não houver raiz, retornar null) - feito
+ */
 package trabalho1;
 
-public class equacao {
-    public static double[] segundoGrau(double a, double b, double c) {
-        a = 3;
-        b= 1;
-        c =3;
-        
-        if (a == 0) {
-            System.out.println("Nao e uma equacao de segundo grau.");
-            return null;
+public class bhaskara {
+    public static void main(String[] args) {
+        double[] raizes = calcularRaizes(1, -3, 2); 
+        // a = calcularRaizes[0]  b = calcularRaizes[1]  c = calcularRaizes[2]
+        if (raizes == null) {
+            System.out.println("Não há raízes para essa equacao");
         } else {
-            if (b == 0 || c == 0) {
-                System.out.println("Equacao incompleta");
-                return ;
+            if(raizes.length == 1) {
+                System.out.println("Há apenas 1 raiz para essa equacao");
+                System.out.println("Raiz = " + raizes[0]);
+            } else {
+                if(raizes.length == 2) {
+                    System.out.println("Há 2 raizes para essa equacao");
+                    System.out.println("Raiz 1 = " + raizes[0] + " Raiz 2 = " + raizes[1]);
+                }
             }
+        }
+    }
 
-    } 
+    public static double[] calcularRaizes(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+
+        // Sem raiz
+        if (delta < 0) {
+            return null;
+        // Ujma raiz
+        } else if (delta == 0) {
+            double raiz = -b / (2 * a);
+            return new double[]{raiz};
+        // Duas raizes
+        } else {
+            double raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{raiz1, raiz2};
+        }
+    }
 }
