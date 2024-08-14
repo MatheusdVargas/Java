dc8a430386a1b487b837ae628d2c6ee4bd5a2601
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-08-14 14:55:02 -0300

    Update Exercicio05.java

--- Subrotinas/ListaSubRot/Exercicio05.java
+++ Subrotinas/ListaSubRot/Exercicio05.java
@@ -1,20 +1,39 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+Escreva um novo programa, alterando o exercício 4, e conte também quantos dos 50 números
+estão abaixo da média calculada.
  */
 package arrayemetodos;
 
-/**
- *
- * @author 05138300067
- */
+import java.util.Scanner;
+
 public class Exercicio05 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leitor = new Scanner(System.in);
+        int acima = 0;
+        int abaixo = 0;
+        double lista[] = new double[50];
+        double soma = 0;
+        double quant = 0;
+        for(int i = 0; i < lista.length; i++) {
+            lista[i] = leitor.nextDouble();
+            quant++;
+            soma += lista[i];
+        }
+        double media = soma / quant;
+        for(int i =0; i<lista.length; i++){
+            if(lista[i] > media){
+                acima++;
+            }
+        for(int j =0; j<lista.length; j++){
+            if(lista[i] < media){
+                abaixo++;
+            }
+            
     }
-    
+            System.out.println(media);
+            System.out.println(acima);
+            System.out.println(abaixo);
+    }
+}
 }
