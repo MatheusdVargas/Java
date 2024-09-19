c7457cd662d90437328ace005ee7b4cf4e397176
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-09-19 11:55:10 -0300

    Add files via upload

---
+++ Sort/QuickSort/QuickSort.java
@@ -0,0 +1,31 @@
+public class QuickSort {
+    public static int partition(int vetor[], int inicio, int fim) {
+            int pivo = vetor[fim];
+            int q = inicio-1;
+            int r;
+            
+            for(r = inicio; r < fim; r++) {
+                if(vetor[r] <= pivo) {
+                    q++;
+                    int temp = vetor[q];
+                    vetor[q] = vetor[r];
+                    vetor[r] = temp;
+                }
+            }
+            int temp = vetor[fim];
+            vetor[fim] = vetor[q+1];
+            vetor[q+1] = temp;
+            return q+1;
+    }
+    public static void sort(int vetor[], int inicio, int fim) {
+        if(inicio < fim) {
+            int meio = partition(vetor, inicio, fim);
+            sort(vetor, inicio, meio -1);
+            sort(vetor, meio+1, fim);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+    
+}
